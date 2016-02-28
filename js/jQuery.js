cd62a3035c5455554aed1a4d6c4c16ee12ba0514
llwang8 <wangxbl56@gmail.com>
2016-02-27 22:32:26 -0500

    redo layout of page, show more weather data

--- js/jQuery.js
+++ js/jQuery.js
@@ -13,23 +13,45 @@ var myDate = new Date();
 var displayDate = myDate.getFullYear() + '.' + myDate.getMonth()+ '.' + myDate.getDate() + ' ' + formatAMPM(myDate);
 //console.log(displayDate);
 
-    $(document).ready(function(){
-        $.getJSON("http://api.openweathermap.org/data/2.5/weather?zip=08540,us&units=imperial&appid=c3a2b88d0195ab9136f72c2270320a91",
-            function(json){
-            var html = "";
-            //json.forEach(function(val){
-                //var keys = Object.keys(val);
+$(document).ready(function(){
+    $.getJSON("http://api.openweathermap.org/data/2.5/weather?zip=08540,us&units=imperial&appid=c3a2b88d0195ab9136f72c2270320a91",
+        function(json){
+        var html = "";
+        var temp = parseFloat(json.main.temp);
+        var htmlTemp = "<div class='icon weather'><img src='http://openweathermap.org/img/w/" + json.weather[0].icon + ".png'>";
+        htmlTemp += temp + " &#8457;&nbsp;&nbsp&nbsp;<button id='changeTemp' class='bt bt-primary bt-temp'>To &#8451;</button><br></div>";
+        $(".message").html(htmlTemp);
 
-                html += "<div class='icon text-center weather'><img src='http://openweathermap.org/img/w/" + json.weather[0].icon + ".png'>"
-                html += json.main.temp; + "</div>"
-                html += "<div class='weather'>" + json.weather[0].description + "<br><br></div>"
-                html += "<div class='datetime'><p>Get at:  " + displayDate + "</p></div>"
-            //});
+        var html = "";
+        html += "<div class='datetime'>" + json.weather[0].main + "</div>";
+        html += "<div class='datetime'><p>Get at:  " + displayDate + "</p></div>";
 
-        $(".message").html(html);
+        $(".message2").html(html);
 
+        //function toggle(t) {
+        $("#changeTemp").on("click", function(){
+            htmlTemp = "<div class='icon weather'><img src='http://openweathermap.org/img/w/" + json.weather[0].icon + ".png'>";
+            if (this.innerHTML() == "To &#8451;") { //celsius
+                temp = temp * 9/5 + 32;
+                htmlTemp += temp + " &#8457;&nbsp;&nbsp&nbsp;<button id='changeTemp' class='bt bt-primary bt-temp' onClick='toggle(this)'>To &#8451;</button><br><br></div>";
+            } else if (this.innerHTML() == "To &#8457") {//farenhai
+                temp = (temp - 32) / (9/5);
+                htmlTemp += temp + " &#8451;&nbsp;&nbsp&nbsp;<button id='changeTemp' class='bt bt-primary bt-temp' onClick='toggle(this)'>To &#8457;</button><br><br></div>";
+            }
+            $(".message").html(htmlTemp);
         });
+
+        var html2 = "";
+        html2 = "<table><tr><td>Wind<td><td>" + json.wind.speed + " m/s<td></tr>"
+        html2 += "<tr><td>Cloudiness<td><td>" + json.weather[0].description + "<td></tr>"
+        html2 += "<tr><td>Pressure<td><td>" + json.main.pressure + " npa<td></tr>"
+        html2 += "<tr><td>Humidity<td><td>" + json.main.humidity + " %<td></tr>"
+        html2 += "<tr><td>Geo coords<td><td>[" + json.coord.lon + ", " + json.coord.lat + "]<td></tr></table>"
+
+        $(".table").html(html2);
+
     });
+});
 
 //google map api with openWeather map layer
 /*
