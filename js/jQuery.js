6f1169574b63b9aef93bfb5c0cbed52d5789c535
llwang8 <wangxbl56@gmail.com>
2016-03-03 22:51:28 -0500

    rework on temperature toggle button

--- js/jQuery.js
+++ js/jQuery.js
@@ -17,41 +17,65 @@ $(document).ready(function(){
     $.getJSON("http://api.openweathermap.org/data/2.5/weather?zip=08540,us&units=imperial&appid=c3a2b88d0195ab9136f72c2270320a91",
         function(json){
         var html = "";
-        var temp = parseFloat(json.main.temp);
-        var htmlTemp = "<div class='icon weather'><img src='http://openweathermap.org/img/w/" + json.weather[0].icon + ".png'>";
-        htmlTemp += temp + " &#8457;&nbsp;&nbsp&nbsp;<button id='changeTemp' class='bt bt-primary bt-temp'>To &#8451;</button><br></div>";
-        $(".message").html(htmlTemp);
+        var tempC = parseFloat(json.main.temp).toFixed(2);
+        var tempF = (parseFloat(json.main.temp) * 9/5 + 32).toFixed(2);
 
-        var html = "";
-        html += "<div class='datetime'>" + json.weather[0].main + "</div>";
-        html += "<div class='datetime'><p>Get at:  " + displayDate + "</p></div>";
+        var htmlTemp = "<div id='f' class='icon weather' style='display:block;'><img src='http://openweathermap.org/img/w/" + json.weather[0].icon + ".png'>";
+        htmlTemp += tempF + " &#8457;&nbsp;&nbsp;&nbsp;<button id='changeTempF' onClick='toggleTemp()' class='bt bt-primary bt-temp' >To &#8451;</button><br></div>";
 
-        $(".message2").html(html);
+        htmlTemp += "<div id='c' class='icon weather' style='display:none;'><img src='http://openweathermap.org/img/w/" + json.weather[0].icon + ".png'>";
+        htmlTemp += tempC + " &#8451;&nbsp;&nbsp;&nbsp;<button id='changeTempC' onClick='toggleTemp()' class='bt bt-primary bt-temp' >To &#8457;</button><br></div>";
 
-        //function toggle(t) {
-        $("#changeTemp").on("click", function(){
-            htmlTemp = "<div class='icon weather'><img src='http://openweathermap.org/img/w/" + json.weather[0].icon + ".png'>";
-            if (this.innerHTML() == "To &#8451;") { //celsius
-                temp = temp * 9/5 + 32;
-                htmlTemp += temp + " &#8457;&nbsp;&nbsp&nbsp;<button id='changeTemp' class='bt bt-primary bt-temp' onClick='toggle(this)'>To &#8451;</button><br><br></div>";
-            } else if (this.innerHTML() == "To &#8457") {//farenhai
-                temp = (temp - 32) / (9/5);
-                htmlTemp += temp + " &#8451;&nbsp;&nbsp&nbsp;<button id='changeTemp' class='bt bt-primary bt-temp' onClick='toggle(this)'>To &#8457;</button><br><br></div>";
-            }
-            $(".message").html(htmlTemp);
-        });
+        htmlTemp += "<div class='datetime'>" + json.weather[0].main + "</div>";
+        htmlTemp += "<div class='datetime'><p>Get at:  " + displayDate + "</p></div>";
+
+         $(".message").html(htmlTemp);
 
         var html2 = "";
-        html2 = "<table><tr><td>Wind<td><td>" + json.wind.speed + " m/s<td></tr>"
-        html2 += "<tr><td>Cloudiness<td><td>" + json.weather[0].description + "<td></tr>"
-        html2 += "<tr><td>Pressure<td><td>" + json.main.pressure + " npa<td></tr>"
-        html2 += "<tr><td>Humidity<td><td>" + json.main.humidity + " %<td></tr>"
-        html2 += "<tr><td>Geo coords<td><td>[" + json.coord.lon + ", " + json.coord.lat + "]<td></tr></table>"
+        html2 = "<table><tr><td>Wind<td><td>" + json.wind.speed + " m/s<td></tr>";
+        html2 += "<tr><td>Cloudiness<td><td>" + json.weather[0].description + "<td></tr>";
+        html2 += "<tr><td>Pressure<td><td>" + json.main.pressure + " npa<td></tr>";
+        html2 += "<tr><td>Humidity<td><td>" + json.main.humidity + " %<td></tr>";
+        html2 += "<tr><td>Geo coords<td><td>[" + json.coord.lon + ", " + json.coord.lat + "]<td></tr></table>";
 
         $(".table").html(html2);
 
     });
 });
+
+function toggleTemp() {
+    var f = document.getElementById("f");
+    var c = document.getElementByID("c");
+
+    f.style.display = (f.style.display === "block" ? "none" : "block");
+    c.style.display = (c.style.display === "block" ? "none" : "block");
+
+}
+
+/*
+
+
+     document.getElementById("f").display = "none";
+        document.getElementByID("c").display = "block";
+    });
+    $("#changeTempC").click(function(){
+        document.getElementById("f").display = "block";
+        document.getElementByID("c").display = "none";
+    });
+
+
+<script type="text/javascript">
+$(document).ready(function(){
+  $("#changeTempF").click(function() {
+        $("#f").hide();
+        $("#c").show();
+    });
+    $("#changeTempC").click(function(){
+        $("#f").show();
+        $("#c").hide();
+    });
+});
+*/
 
 //google map api with openWeather map layer
 /*
@@ -73,8 +97,5 @@ $(document).ready(function(){
      request.open("get", requestString, true);
 
 */
-
-
-
 
 //http://api.openweathermap.org/data/2.5/weather?zip=08540,us&appid=c3a2b88d0195ab9136f72c2270320a91
