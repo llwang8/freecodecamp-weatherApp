b03e9d1baaf17c0d8c42d7fff1450cce5c1f80f9
llwang8 <wangxbl56@gmail.com>
2016-08-01 21:34:28 -0400

    fix temp calculation and reorganize style

---
+++ README.md
@@ -0,0 +1,14 @@
+## FreeCodeCamp: Build a Local Weather Viewer
+
+I built a Local Weather Viewer with JavaScript, jQuery, HTML and CSS, and Open Weather API, while following some best practices for structuring HTML, writing efficient CSS, and commenting code along the way.
+
+ User can see the weather in a set location, see a different icon or background image (e.g. snowy mountain, hot desert) depending on the weather and push a button to toggle between Fahrenheit and Celsius.
+
+#Built with:
+
+Implemented with JavaScript, jQuery, HTML and CSS. Also used Open Weather API, digital color meter.
+
+# Link
+
+You can view it on codepan:
+- [Local Weather Viewer](http://codepen.io/llwang8/pen/grOerO)
--- js/jQuery.js
+++ js/jQuery.js
@@ -1,29 +1,20 @@
-function formatAMPM(date) { // This is to display 12 hour format like you asked
-  var hours = date.getHours();
-  var minutes = date.getMinutes();
-  var ampm = hours >= 12 ? 'pm' : 'am';
-  hours = hours % 12;
-  hours = hours ? hours : 12; // the hour '0' should be '12'
-  minutes = minutes < 10 ? '0'+minutes : minutes;
-  var strTime = hours + ':' + minutes + ' ' + ampm;
-  return strTime;
-}
+
 
 var myDate = new Date();
-var displayDate = myDate.getFullYear() + '.' + myDate.getMonth()+ '.' + myDate.getDate() + ' ' + formatAMPM(myDate);
+var displayDate = myDate.getFullYear() + '.' + eval(myDate.getMonth() + 1)+ '.' + myDate.getDate() + ' ' + formatAMPM(myDate);
 //console.log(displayDate);
 
 $(document).ready(function(){
     $.getJSON("http://api.openweathermap.org/data/2.5/weather?zip=08540,us&units=imperial&appid=c3a2b88d0195ab9136f72c2270320a91",
         function(json){
         var html = "";
-        var tempC = parseFloat(json.main.temp).toFixed(2);
-        var tempF = (parseFloat(json.main.temp) * 9/5 + 32).toFixed(2);
+        var tempF = parseFloat(json.main.temp).toFixed(1);
+        var tempC = (parseFloat(json.main.temp - 32) * 5/9).toFixed(1);
 
-        var htmlTemp = "<div id='f' class='icon weather' style='display:block;'><img src='http://openweathermap.org/img/w/" + json.weather[0].icon + ".png'>";
+        var htmlTemp = "<div id='f' class='weather' style='display:block;'><img src='http://openweathermap.org/img/w/" + json.weather[0].icon + ".png'>";
         htmlTemp += tempF + " &#8457;&nbsp;&nbsp;&nbsp;<button id='changeTempF' onClick='toggleTemp()' class='bt bt-primary bt-temp' >To &#8451;</button><br></div>";
 
-        htmlTemp += "<div id='c' class='icon weather' style='display:none;'><img src='http://openweathermap.org/img/w/" + json.weather[0].icon + ".png'>";
+        htmlTemp += "<div id='c' class='weather' style='display:none;'><img src='http://openweathermap.org/img/w/" + json.weather[0].icon + ".png'>";
         htmlTemp += tempC + " &#8451;&nbsp;&nbsp;&nbsp;<button id='changeTempC' onClick='toggleTemp()' class='bt bt-primary bt-temp' >To &#8457;</button><br></div>";
 
         htmlTemp += "<div class='datetime'>" + json.weather[0].main + "</div>";
@@ -34,7 +25,7 @@ $(document).ready(function(){
         var html2 = "";
         html2 = "<table><tr><td>Wind<td><td>" + json.wind.speed + " m/s<td></tr>";
         html2 += "<tr><td>Cloudiness<td><td>" + json.weather[0].description + "<td></tr>";
-        html2 += "<tr><td>Pressure<td><td>" + json.main.pressure + " npa<td></tr>";
+        html2 += "<tr><td>Pressure<td><td>" + json.main.pressure + " hpa<td></tr>";
         html2 += "<tr><td>Humidity<td><td>" + json.main.humidity + " %<td></tr>";
         html2 += "<tr><td>Geo coords<td><td>[" + json.coord.lon + ", " + json.coord.lat + "]<td></tr></table>";
 
@@ -42,6 +33,17 @@ $(document).ready(function(){
 
     });
 });
+
+function formatAMPM(date) { // This is to display 12 hour format
+  var hours = date.getHours();
+  var minutes = date.getMinutes();
+  var ampm = hours >= 12 ? 'pm' : 'am';
+  hours = hours % 12;
+  hours = hours ? hours : 12; // the hour '0' should be '12'
+  minutes = minutes < 10 ? '0'+minutes : minutes;
+  var strTime = hours + ':' + minutes + ' ' + ampm;
+  return strTime;
+}
 
 function toggleTemp() {
 
